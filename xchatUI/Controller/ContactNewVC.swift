e28c97bd08a2fc1faf2a758d2baa42192a78b5cc
Tiago Do Couto <cs.couto@outlook.com>
2018-02-25 10:27:26 -0600

    [storyboard] - completed layout for new contact

---
+++ xchatUI/Controller/ContactNewVC.swift
@@ -0,0 +1,15 @@
+//
+//  ContactNewVC.swift
+//  allnet-xchat
+//
+//  Created by Tiago Do Couto on 2/25/18.
+//  Copyright © 2018 allnet. All rights reserved.
+//
+
+class ContactNewVC: UIViewController {
+    @IBOutlet weak var textFieldName: UITextField!
+    @IBOutlet weak var textFieldSecret: UITextField!
+    @IBOutlet weak var pickerViewConnection: UIPickerView!
+    @IBOutlet weak var tableView: UITableView!
+    
+}
